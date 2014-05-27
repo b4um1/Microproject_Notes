474e58d6c0f309da3529c49698abedd0b027f508
s1310237003 <s1310237003@students.fh-hagenberg.at>
2014-05-27 09:19:55 +0200

    First commit

---
+++ Microproject_Nots/src/at/fh/ooe/mc/android/MainActivity.java
@@ -0,0 +1,23 @@
+package at.fh.ooe.mc.android;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import at.fh.ooe.mc.androi.R;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+	}
+}
