1133acbecfac9828dfcfdc112b08c82e109d542f
t.kranzer <t.kranzer@gmail.com>
2014-05-27 11:33:12 +0200

    add note

---
+++ Microproject_Nots/src/at/fh/ooe/mc/android/AddNoteActivity.java
@@ -0,0 +1,25 @@
+package at.fh.ooe.mc.android;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import at.fh.ooe.mc.androi.R;
+import at.fh.ooe.mc.androi.R.layout;
+
+public class AddNoteActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_add_note);
+
+		
+	}
+}
