5912c3551e2fa5d12ead21301996a21668ede7b0
s1310237003 <s1310237003@students.fh-hagenberg.at>
2014-05-27 10:11:09 +0200

    Commit MainActivity with OnClickListener

--- Microproject_Nots/src/at/fh/ooe/mc/android/MainActivity.java
+++ Microproject_Nots/src/at/fh/ooe/mc/android/MainActivity.java
@@ -1,24 +1,25 @@
 package at.fh.ooe.mc.android;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import at.fh.ooe.mc.androi.R;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		// comment
+		
+		Button b = (Button) findViewById(R.id.button1);
+		b.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
 	}
 }
