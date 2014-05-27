392013cba5294e7b1047d820c59a321702f166f4
s1310237003 <s1310237003@students.fh-hagenberg.at>
2014-05-27 11:36:43 +0200

    commit ActionBar symbol, AddNoteActivity

--- Microproject_Nots/src/at/fh/ooe/mc/android/MainActivity.java
+++ Microproject_Nots/src/at/fh/ooe/mc/android/MainActivity.java
@@ -4,8 +4,12 @@ package at.fh.ooe.mc.android;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.Toast;
 import at.fh.ooe.mc.androi.R;
 
 public class MainActivity extends Activity{
@@ -27,5 +31,18 @@ public class MainActivity extends Activity{
 		
 		ListView v = (ListView)findViewById(R.id.container);
 		v.setAdapter(adapter);
+	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu _menu) {
+		getMenuInflater().inflate(R.menu.menu, _menu);
+		return super.onCreateOptionsMenu(_menu);
+	}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem _item) {
+		Toast.makeText(getApplicationContext(), _item.getTitle(),
+				Toast.LENGTH_SHORT).show();
+		Intent i = new Intent (this,AddNoteActivity.class);
+		this.startActivity(i);
+		return super.onOptionsItemSelected(_item);
 	}
 }
