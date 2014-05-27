70d30272f97d1baec66f1219899654ea8df4ae4f
s1310237003 <s1310237003@students.fh-hagenberg.at>
2014-05-27 11:08:46 +0200

    commit ListView, ArrayAdapter, Notes

--- Microproject_Nots/src/at/fh/ooe/mc/android/MainActivity.java
+++ Microproject_Nots/src/at/fh/ooe/mc/android/MainActivity.java
@@ -1,25 +1,30 @@
 package at.fh.ooe.mc.android;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.widget.ListView;
 import at.fh.ooe.mc.androi.R;
 
-public class MainActivity extends Activity implements OnClickListener{
+public class MainActivity extends Activity{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
 		
-		Button b = (Button) findViewById(R.id.button1);
-		b.setOnClickListener(this);
-	}
-
-	@Override
-	public void onClick(View v) {
+		ArrayList<Note> list = new ArrayList<Note>();
+		Note c1 = new Note ("Titel 1", "Note blabldaslkfjaslf");
+		Note c2 = new Note ("Titel 2", "Note blabldaslkfjaslf");
+		Note c3 = new Note ("Titel 3", "Note blabldaslkfjaslf");
+		list.add(c1);
+		list.add(c2);
+		list.add(c3);
+		
+		MyArrayAdapter adapter = new MyArrayAdapter(this, R.layout.mainlist_layout, list);
+		
+		ListView v = (ListView)findViewById(R.id.container);
+		v.setAdapter(adapter);
 	}
 }
