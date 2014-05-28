95ca9b3c6d9affae5324b6b5c59c802207686526
t.kranzer <t.kranzer@gmail.com>
2014-05-28 16:33:58 +0200

    commit

--- Microproject_Nots/src/at/fh/ooe/mc/android/AddNoteActivity.java
+++ Microproject_Nots/src/at/fh/ooe/mc/android/AddNoteActivity.java
@@ -238,24 +238,14 @@ public class AddNoteActivity extends Activity implements OnClickListener {
 								+ selectedYear + "/" + selectedHour + "/"
 								+ selectedMin);
 
-						// Intent i = new Intent(this.getApplicationContext(),
-						// NotificationActivity.class);
-						// PendingIntent pi = PendingIntent.getActivity(this, 0,
-						// i, 0);
-						// mgr.set(AlarmManager.RTC_WAKEUP,
-						// System.currentTimeMillis() + 10000, pi);
-
-//						Intent myIntent = new Intent(ThisApp.this , myService.class);     
-//					       AlarmManager alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
-//					       pendingIntent = PendingIntent.getService(ThisApp.this, 0, myIntent, 0);
-//
-//					       Calendar calendar = Calendar.getInstance();
-//					           calendar.set(Calendar.HOUR_OF_DAY, 12);
-//					       calendar.set(Calendar.MINUTE, 00);
-//					       calendar.set(Calendar.SECOND, 00);
-//
-//					      alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), 24*60*60*1000 , pendingIntent);
-
+						 Intent i = new Intent(this.getApplicationContext(),
+						 NotificationActivity.class);
+						 PendingIntent pi = PendingIntent.getActivity(this, 0,
+						 i, 0);
+						 mgr.set(AlarmManager.RTC_WAKEUP,
+						 System.currentTimeMillis() + 10000, pi);
+						 
+						 
 					}
 					helper.addNote(note);
 
